818163f3e9ad03a10571d1f3c06f9e89b3249b63
Glitch (heardle-kpop) <none>
2022-03-21 12:21:36 +0000

    🍮💄 Checkpoint ./main.js:43068995/333

--- main.js
+++ main.js
@@ -597,7 +597,7 @@ var app = (function () {
       c() {
         (t = k("svg")),
           (n = k("path")),
-          M(n, "d", "M12 20v-6M6 20V10M18 20V4"),
+          M(n, "d", "M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"),
           M(t, "xmlns", "http://www.w3.org/2000/svg"),
           M(t, "width", "24"),
           M(t, "height", "24"),
